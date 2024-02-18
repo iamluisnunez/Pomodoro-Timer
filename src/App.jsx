7efed59752c5ae961e5f86501b4676bfc4b203b8
iamluisnunez <luis.nunez@mykenzie.snhu.edu>
2024-02-17 20:40:22 -0600

    editing routes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
 
   return (
     <>
-      <AppRoutes />
       <main>
         <SettingsContext.Provider
           value={{
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,7 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Timer from "./Timer";
 import Test from "./Test";
 import Nav from "./Nav";
+import SettingsContext from "./SettingsContext";
+import { useState } from "react";
+
 const AppRoutes = () => {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(25);
   return (
     <Router>
       <Routes>
@@ -10,6 +16,18 @@ const AppRoutes = () => {
         <Route path="/test" element={<Test />} />
       </Routes>
       <Nav />
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
     </Router>
   );
 };
